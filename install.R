1bfa4b60c32d8999f092c883a061ae4a48d0a340
J aristia <j.aristia@gmail.com>
2024-10-08 17:07:12 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,8 @@
 install.packages("readr")
+install.packages("servr")
+install.packages("dplyr")
+install.packages("stringi")
+install.packages("ggplot2")
+install.packages("tidytext")
+install.packages("tidyverse")
+install.packages("readxl")
