8dfd9adcb7c9f5481c149fffcbb4de1e728fa4cb
J aristia <j.aristia@gmail.com>
2024-10-08 14:57:34 +0100

    Add files via upload

---
+++ pt_pronoun_bayes_mdl.R
@@ -0,0 +1,18 @@
+library(readxl)
+
+df <- read_excel("C:/Users/jaris/Documents/PT_Bayes_Model/quest_EP_BP_pronouns.xlsx")
+
+prob_m_c <- function (subj, obj) { 
+ subject = subj/100
+ object = obj/100
+ return(c(subject, object))}
+
+subject<- prob_m_c(50,50)[1]
+object<- prob_m_c(50,50)[2]
+
+prob_2 <- function (dframe, column, covert_subj, overt_subj, covert_obj, overt_obj) {
+  vec <- dframe[,column]
+  dframe$Prob_F_MrCq <- ifelse (vec == "object", subject*(overt_subj/covert_obj),
+                                object*(covert_subj/overt_obj))
+  return(dframe)
+}
